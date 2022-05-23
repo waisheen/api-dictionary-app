a3f17159e2d6074622dace9dfcb982858b6969aa
Wai Sheen Chong <chong.wai.sheen@gmail.com>
2022-05-24 01:12:13 +0800

    make query

--- app/src/main/java/com/example/dictionary/Components/Definition.java
+++ app/src/main/java/com/example/dictionary/Components/Definition.java
@@ -1,8 +1,12 @@
 package com.example.dictionary.Components;
 
+import java.util.List;
+
 public class Definition {
     String definition = "";
     String example = "";
+    List<String> synonyms = null;
+    List<String> antonyms = null;
 
     public void setDefinition(String definition) {
         this.definition = definition;
@@ -12,11 +16,27 @@ public class Definition {
         this.example = example;
     }
 
+    public void setSynonyms(List<String> synonyms) {
+        this.synonyms = synonyms;
+    }
+
+    public void setAntonyms(List<String> antonyms) {
+        this.antonyms = antonyms;
+    }
+
     public String getDefinition() {
         return this.definition;
     }
 
     public String getExample() {
         return this.example;
+    }
+
+    public List<String> getSynonyms() {
+        return synonyms;
+    }
+
+    public List<String> getAntonyms() {
+        return antonyms;
     }
 }
--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dictionary.Adapters.MeaningsAdapter;
+import com.example.dictionary.Adapters.PhoneticsAdapter;
+import com.example.dictionary.Components.APIResponse;
 
 public class MainActivity extends AppCompatActivity {
+    SearchView search_view;
+    TextView textView_word;
+    RecyclerView recycler_phonetics;
+    //TextView textView_meaning;
+    RecyclerView recycler_meaning;
+    ProgressDialog progressDialog;
+    PhoneticsAdapter phoneticsAdapter;
+    MeaningsAdapter meaningsAdapter;
+
+    private final OnFetchDataListener listener = new OnFetchDataListener() {
+        @Override
+        public void onFetchData(APIResponse apiResponse, String message) {
+            progressDialog.dismiss();
+            if (apiResponse == null) {
+                Toast.makeText(MainActivity.this, "No data found :(", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            showData(apiResponse);
+        }
+
+        @Override
+        public void onError(String message) {
+            progressDialog.dismiss();
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void showData(APIResponse apiResponse) {
+        textView_word.setText("Word: " + apiResponse.getWord());
+
+        recycler_phonetics.setHasFixedSize(true);
+        recycler_phonetics.setLayoutManager(new GridLayoutManager(this, 1));
+        phoneticsAdapter = new PhoneticsAdapter(this, apiResponse.getPhonetics());
+        recycler_phonetics.setAdapter(phoneticsAdapter);
+
+        recycler_meaning.setHasFixedSize(true);
+        recycler_meaning.setLayoutManager(new GridLayoutManager(this, 1));
+        meaningsAdapter = new MeaningsAdapter(this, apiResponse.getMeanings());
+        recycler_meaning.setAdapter(meaningsAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_view = findViewById(R.id.search_view);
+        textView_word = findViewById(R.id.textView_word);
+        recycler_phonetics = findViewById(R.id.recycler_phonetics);
+        recycler_meaning = findViewById(R.id.recycler_meaning);
+        progressDialog = new ProgressDialog(this);
+
+        progressDialog.setTitle("Loading...");
+        progressDialog.show();
+        RequestManager manager = new RequestManager(MainActivity.this);
+        manager.getWordMeaning(listener, "hello");
+
+
+        search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                progressDialog.setTitle("Fetching data for " + query);
+                progressDialog.show();
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getWordMeaning(listener, query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
